d956d4783e74b86686df04ee4615ee4505acc04e
Sylvain Decout <sdecout@xebia.fr>
2020-05-18 18:49:46 +0200

    Replace JUnit4 with JUnit5

--- src/test/java/com/pse/coffee/AppTest.java
+++ src/test/java/com/pse/coffee/AppTest.java
@@ -1,24 +1,23 @@
 package com.pse.coffee;
 
-import com.pse.coffee.domain.CommandHandler;
 import com.pse.coffee.domain.model.CoffeeOrder;
 import com.pse.coffee.domain.model.CoffeeType;
 import com.pse.coffee.infra.driving.UserCommandHandler;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@ExtendWith(SpringExtension.class)
 @ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = {SpringContextConfiguration.class})
-public class AppTest {
+class AppTest {
     @Autowired
     private UserCommandHandler userCommandHandler;
 
     @Test
-    public void appTest() {
+    void appTest() {
         userCommandHandler.handleUserCommand(new CoffeeOrder(CoffeeType.LATTE));
     }
 }
